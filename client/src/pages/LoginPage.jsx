0bba624f7a65328bbacba570299db741470bae61
noke1 <mateusz.kaniecki1@gmail.com>
2024-02-04 22:29:53 +0100

    Create templates for login and register forms

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
+
 function LoginPage() {
-  return `login page here!`
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-48">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="primary">Login</button>
+          <div className="text-center pt-3 mx-auto">
+            Don't have an account? Register{' '}
+            <Link to={'/register'} className="font-bold">
+              here
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
 function RegisterPage() {
-  return `This is the register page!`
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-48">
+        <h1 className="text-4xl text-center mb-4">Register an account</h1>
+        <form className="max-w-md mx-auto">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="primary">Register</button>
+          <div className="text-center pt-3 mx-auto">
+            Already have an account? Login{' '}
+            <Link to={'/login'} className="font-bold">
+              here
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default RegisterPage
